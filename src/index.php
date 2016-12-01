5d0f27cacbc009cdc46153910891168137fd528a
Surakit Choodet <kuakling@gmail.com>
2016-12-01 18:37:37 +0700

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "mysql"; //can't use localhost. Must use mysql is a mysql name docker-compose.yml
+$username = "root";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
