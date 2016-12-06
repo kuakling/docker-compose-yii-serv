bea49f904417cb249aac9450f499bcbd36126cc7
Surakit Choodet <surakit@Surakits-MacBook-Pro.local>
2016-12-06 12:05:41 +0700

    Ok

---
+++ build/php/composer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Register personal github token
+# Example: composer config -g github-oauth.github.com 1234567890abcdefghijklmnopqrstuvwxyz
+composer config -g github-oauth.github.com YOUR GITHUB TOKEN
+
+rm -rf /var/www/html/*
+
+
--- cmd/clean.sh
+++ cmd/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -rf ./tmp/mysql
+rm -rf ./tmp/database
